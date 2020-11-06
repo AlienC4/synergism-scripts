88c812863a25d13deef19fbb9cbf36915ad31c15
AlienC4 <20047400+alienc4@users.noreply.github.com>
2020-11-06 20:45:08 +0200

    Added platonics per second to the ascend logger

--- CubesPerSecDisplay.user.js
+++ CubesPerSecDisplay.user.js
@@ -17,23 +17,24 @@ let calcCTHps = (function () {
     let cps = 0;
     let tps = 0;
     let hps = 0;
+    let pps = 0;
     let sec = 0;
 
     function log() {
         let meta = CalcCorruptionStuff();
+        let platonics = meta[7];
         let hypers = meta[6];
         let tess = meta[5];
         let cubes = meta[4];
         let time = player.ascensionCounter;
         if (time > 0 && sec >= time && printPrevRun) {
-            console.log(`Cube Rate: ${format(cps, 4, true)}/s, Tess Rate: ${format(tps, 2, true)}/s, Hyper Rate: ${format(hps, 3, true)}/s, Cubes: ${format(cps * sec, 4, true)}, time: ${formatTimeShort(sec)}`);
+            console.log(`Cube Rate: ${format(cps, 4, true)}/s, Tess Rate: ${format(tps, 2, true)}/s, Hyper Rate: ${format(hps, 3, true)}/s, Platonic rate: ${format(pps, 4, true)}, Cubes: ${format(cps * sec, 4, true)}, time: ${formatTimeShort(sec)}`);
         }
         cps = cubes / time;
         tps = tess / time;
         hps = hypers / time;
+        pps = platonics / time;
         sec = time;
-        if (document.getElementsByTagName("div")[2].childElementCount === 0)
-            document.getElementsByTagName("div")[2].textContent = `Cube Rate: ${format(cps, 4, true)}/s, Tess Rate: ${format(tps, 2, true)}/s, Hyper Rate: ${format(hps, 3, true)}/s, Cubes: ${format(cubes, 4, true)}, time: ${formatTimeShort(time)}`;
     }
 
     return log;
